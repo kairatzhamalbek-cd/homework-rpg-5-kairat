a6369d06be19b27d398c1001bc203bcb14287361
kairatzhamalbek-cd <kairat.zhamalbek@narxoz.kz>
2026-03-13 20:31:16 +0600

    Добавлен RewardService для выдачи награды после боя

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,14 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward";
         }
-        return "TODO";
-    }
-}
+        if ("Draw".equals(battleResult.getWinner())) {
+            return "No reward";
+        }
+
+
+        return "Treasure Chest";
+
+    }}
